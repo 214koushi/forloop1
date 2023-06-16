c243ec9c9d100af5d5510d9c5146ccd9eed28feb
214koushi <127491423+214koushi@users.noreply.github.com>
2023-06-16 13:23:02 +0530

    Update a7.java

--- forloop/src/forloop/a7.java
+++ forloop/src/forloop/a7.java
@@ -3,7 +3,7 @@ package forloop;
 public class a7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 for (int i = 0; i <4; i++) {
 	for (int j = 0; j <4; j++) {
 		System.out.print("*");
